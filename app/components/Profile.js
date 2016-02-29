8c24c9c1c142df402d72ac1e0ded39789c1204d9
imorenodev <imorenodev@gmail.com>
2016-02-29 15:03:34 -0500

    add firebase and reactfire mixin functionality

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,22 +3,38 @@ var UserProfile = require('./Github/UserProfile');
 var Repos = require('./Github/Repos');
 var Notes = require('./Notes/Notes');
 var Router = require('react-router');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 
 //component will manage user profile, repos, and notes components
 var Profile = React.createClass({
+  //Mixins take the 'this' instance and mixes in functionality adding on methods to use
+  mixins: [ReactFireMixin],
   //method will set the initial state of the component
   getInitialState: function(){
     return {
       notes: [1,2,3],
       bio: {
-        name: 'Ian'
+        name: 'tyler'
       },
       repos: ['a', 'b', 'c']
     };
   },
+  //component lifecycle event will be called as soon as the view is mounted
+  componentDidMount: function(){
+    //returns an object with firebase properties, and save it on 'this' instance under the ref property
+    this.ref = new Firebase('https://intense-heat-9991.firebaseio.com/');
+    //.child is a firebase property, take the root firebase url and grab the username from 'this' state
+    var childRef = this.ref.child(this.props.params.username);
+    //bindAsArray is a firebase method added to the 'this' instance
+    //bindAsArray takes two arguments, a reference to the firebase, and a property on state to bind the firebase data to.
+    this.bindAsArray(childRef, 'notes');
+  },
+  componentWillUnmount: function(){
+    this.unbind('notes');
+  },
 
   render: function(){
-    console.log(this.props)
     return (
       <div className="container">
         <div className="row">
@@ -26,15 +42,15 @@ var Profile = React.createClass({
             <UserProfile username={this.props.params.username} bio={this.state.bio} />
           </div>
           <div className="col-md-4">
-            <Repos repos={this.state.repos} />
+            <Repos username={this.props.params.username} repos={this.state.repos} />
           </div>
           <div className="col-md-4">
-            <Notes notes={this.state.notes} />
+            <Notes username={this.props.params.username} notes={this.state.notes} />
           </div>
         </div>
       </div>
     )
   }
-})
+});
 
 module.exports = Profile;
